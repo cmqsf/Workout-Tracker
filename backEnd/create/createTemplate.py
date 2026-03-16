9dd4c42098b6f0266e74bba06cc4a82b18f32416
cmqsf <cmquarterman@gmail.com>
2026-03-16 16:22:46 -0700

    Templates for requests

---
+++ backEnd/create/createTemplate.py
@@ -0,0 +1,94 @@
+
+from typing import Optional, List, Dict, Union
+from typing_extensions import Literal
+from pydantic import BaseModel
+
+DeadliftType = Union[
+    Literal[
+        "Conventional",
+        "Romanian",
+        "Sumo", 
+        "Single-Leg Romanian",
+        "B-Stance",
+        "Stiff Leg",
+        "Snatch Grip"
+    ]
+]
+
+PressType = Union[
+    Literal[
+        "Barbell Bench",
+        "Barbell Strict", 
+        "Barbell Push",
+        "Dumbbell Bench",
+        "Dumbbell Strict",
+        "Dumbbell Push"
+    ]
+]
+
+SquatType = Union[
+    Literal[
+        "Back",
+        "Front",
+        "Single-Leg",
+        "Goblet",
+        "Hip Thrust"
+    ]
+]
+
+CardioType = Union[
+    Literal[
+        "Run",
+        "Walk", 
+        "Hike",
+        "Row"
+    ]
+]
+
+class LiftInfo(BaseModel): 
+    username: str
+    timestamp: str
+    sets: int
+    reps: int
+    minWeight: Optional[float] = 0.0
+    maxWeight: float
+    allWeights: Optional[List[float]] = []
+    amrap: Optional[Dict[str, Union[int, float]]] = {}
+    pause: Optional[bool] = False
+    estimatedRPE: Optional[float] = 0.0
+    pr: Optional[bool] = False
+
+class Deadlift(LiftInfo): 
+    type: DeadliftType
+
+class Press(LiftInfo): 
+    type: Optional[PressType] = None
+
+class Squat(LiftInfo): 
+    type: Optional[SquatType] = None
+
+class Cardio(BaseModel): 
+    username: str
+    timestamp: str
+    type: CardioType
+    distance: Optional[float] = 0.0
+    hours: Optional[int] = 0
+    minutes: Optional[int] = 0
+    seconds: Optional[int] = 0
+
+class Other(BaseModel): 
+    username: str
+    type: str
+    timestamp: str
+    details: Optional[str] = None
+    weight: Optional[float] = 0.0
+    sets: Optional[int] = 0
+    reps: Optional[int] = 0
+
+class NewUser(BaseModel): 
+    username: str
+    fn: str
+    ln: Optional[str] = None
+    birthday: Optional[str] = None
+    weight: Optional[float] = 0.0
+    height: Optional[float] = 0.0 

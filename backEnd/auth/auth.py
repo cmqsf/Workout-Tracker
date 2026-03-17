0e61df1184137d50173fea9bcee45c59c480040d
cmqsf <cmquarterman@gmail.com>
2026-03-16 22:34:06 -0700

    Stupid auth

---
+++ backEnd/auth/auth.py
@@ -0,0 +1,49 @@
+
+## There are 100 better ways to do auth than this.
+## Don't do this
+## I'm just being lazy
+
+
+import logging
+import os
+from datetime import date
+import json
+import pymongo
+
+from fastapi import Form, APIRouter, HTTPException
+from typing import Annotated
+from contextlib import asynccontextmanager
+
+from data.mongo import get_users_coll
+from create.createTemplate import User
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+coll = get_users_coll()
+
+@asynccontextmanager
+async def router_lifespan(app: APIRouter): 
+    global schema
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    yield
+
+router = APIRouter(lifespan=router_lifespan)
+
+@router.post("/login")
+def login(username: str, password: str): 
+
+    try: 
+
+        user = coll.find_one({"auth.username": username})
+        if not user: 
+            raise HTTPException(status_code=404, detail=f"User {username} not found.")
+        
+        if password != user['auth'].get('password'): 
+            raise HTTPException(status_code=401, detail=f"Password is incorrect.")
+        
+        return True
+    
+    except Exception as e: 
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")

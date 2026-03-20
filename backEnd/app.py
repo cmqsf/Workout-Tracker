3c8cf13f8e4828e0483aab6c5d14fb4a135f6b7b
cmqsf <cmquarterman@gmail.com>
2026-03-20 14:02:52 -0700

    Add new routes to app

--- backEnd/app.py
+++ backEnd/app.py
@@ -7,6 +7,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from user.createUserProfile import router as createUser_route
 from user.updateUserProfile import router as updateUser_route
 from auth.auth import router as auth_route
+from create.createDeadlift import router as deadlift_route
+from create.createPress import router as press_route
+from create.createSquat import router as squat_route
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,6 +28,9 @@ app.add_middleware(
 app.include_router(createUser_route)
 app.include_router(updateUser_route)
 app.include_router(auth_route)
+app.include_router(deadlift_route)
+app.include_router(press_route)
+app.include_router(squat_route)
 
 if __name__ == "__main__": 
     uvicorn.run(app, host='0.0.0.0', port=8000)

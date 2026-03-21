f525308cdc29b2b6c54d09f94ce32c081fefda7d
cmqsf <cmquarterman@gmail.com>
2026-03-20 17:15:18 -0700

    Add return statement

--- backEnd/decorators/updateDLStats.py
+++ backEnd/decorators/updateDLStats.py
@@ -1,5 +1,4 @@
 import functools
-import os
 from data.mongo import get_users_coll
 from create.createTemplate import normalizeType
 import logging
@@ -28,37 +27,28 @@ def updateDLStats(createDeadlift):
                 logger.error("Could not find user")
                 return createDeadlift(*args, **kwargs)
             
-            blank_stats = {
-                "numWorkouts": 0, 
-                "pr": {
-                    "conventional": 0.0,
-                    "romanian": 0.0,
-                    "sumo": 0.0,
-                    "singleLegRomanian": 0.0,
-                    "stiffLeg": 0.0,
-                    "snatchGrip": 0.0,
-                    "bStance": 0.0
-                }
-            }
-            
             workoutStats = user.get('workoutStats')
             if not workoutStats: 
-                workoutStats = {
-                    "deadlifts": blank_stats
-                }
+                pass
 
             deadliftStats = workoutStats.get("numWorkouts")
             if not deadliftStats: 
-                workoutStats['deadlifts'] = blank_stats
-                deadliftStats = blank_stats
+                pass
 
-            currentNumWorkouts = deadliftStats.get('numWorkouts')
+            currentNumWorkouts = deadliftStats.get('numWorkouts', 0)
             if isinstance(currentNumWorkouts, int):
                 update_fields["numWorkouts"] = currentNumWorkouts + 1
 
             dlType = normalizeType(request.type.split(" "))
 
-            update_fields["pr"][dlType] = request.maxWeight           
+            update_fields["pr"][dlType] = request.maxWeight   
+
+            coll.update_one(filter, {"$set": update_fields})
+            logger.info("Successfully updated user stats!") 
+            
+            return createDeadlift(*args, **kwargs)                   
 
         except Exception as e: 
             logger.error(f"Could not update user stats: {e}")
+
+        return wrapper

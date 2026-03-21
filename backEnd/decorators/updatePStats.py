8642e22e25ad3322656cc592242cd41456b7b59b
cmqsf <cmquarterman@gmail.com>
2026-03-20 17:24:00 -0700

    Update correct field

---
+++ backEnd/decorators/updatePStats.py
@@ -0,0 +1,59 @@
+import functools
+from data.mongo import get_users_coll
+from create.createTemplate import normalizeType
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+coll = get_users_coll()
+
+def updateDLStats(createPress): 
+
+    @functools.wraps(createPress)
+    def wrapper(*args, **kwargs): 
+        try: 
+            
+            request = kwargs.get("request")
+            if request is None: 
+                logger.error("Could not find request.")
+                return createPress(*args, **kwargs)
+            
+            username = request.get("username", None)
+            filter = {'auth.username': username}
+            update_fields = {}
+            user = coll.find_one(filter)
+            if not user: 
+                logger.error("Could not find user")
+                return createPress(*args, **kwargs)
+            
+            workoutStats = user.get('workoutStats')
+            if not workoutStats: 
+                pass
+
+            pressStats = workoutStats.get("numWorkouts")
+            if not pressStats: 
+                pass
+
+            currentNumWorkouts = pressStats.get('numWorkouts', 0)
+            if isinstance(currentNumWorkouts, int):
+                update_fields["numWorkouts"] = currentNumWorkouts + 1
+
+            pressType = normalizeType(request.type.split(" "))
+
+            allPrs = pressStats.get("pr")
+            if not allPrs: 
+                pass
+            pr = allPrs.get(pressType)
+            if request.maxWeight > pr:
+                update_fields["press"]["pr"][pressType] = request.maxWeight   
+
+            coll.update_one(filter, {"$set": update_fields})
+            logger.info("Successfully updated user stats!") 
+            
+            return createPress(*args, **kwargs)                   
+
+        except Exception as e: 
+            logger.error(f"Could not update user stats: {e}")
+
+        return wrapper
